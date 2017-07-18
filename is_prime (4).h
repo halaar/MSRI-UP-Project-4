538cb47b0c94b07d529bbb4ab3dd524812a6008d
Aaron Hale <aaron.hale2@gmail.com>
2017-07-18 10:09:21 -0700

    Uploaded files

---
+++ is_prime (4).h
@@ -0,0 +1,76 @@
+//is_prime_mr.h
+
+typedef long long lint;
+
+// calcul a^n%mod
+lint power(lint a, lint n, lint mod)
+{
+    lint power = a;
+    lint result = 1;
+ 
+    while (n)
+    {
+        if (n & 1)
+            result = (result * power) % mod;
+        power = (power * power) % mod;
+        n >>= 1;
+    }
+    return result;
+}
+ 
+// n-1 = 2^s * d with d odd by factoring powers of 2 from n-1
+bool witness(lint n, lint s, lint d, lint a)
+{
+    lint x = power(a, d, n);
+    lint y;
+ 
+    while (s) {
+        y = (x * x) % n;
+        if (y == 1 && x != 1 && x != n-1)
+            return false;
+        x = y;
+        --s;
+    }
+    if (y != 1)
+        return false;
+    return true;
+}
+ 
+/*
+ * if n < 1,373,653, it is enough to test a = 2 and 3;
+ * if n < 9,080,191, it is enough to test a = 31 and 73;
+ * if n < 4,759,123,141, it is enough to test a = 2, 7, and 61;
+ * if n < 1,122,004,669,633, it is enough to test a = 2, 13, 23, and 1662803;
+ * if n < 2,152,302,898,747, it is enough to test a = 2, 3, 5, 7, and 11;
+ * if n < 3,474,749,660,383, it is enough to test a = 2, 3, 5, 7, 11, and 13;
+ * if n < 341,550,071,728,321, it is enough to test a = 2, 3, 5, 7, 11, 13, and 17.
+ */
+ 
+bool is_prime_mr(lint n)
+{
+    if (((!(n & 1)) && n != 2 ) || (n < 2) || (n % 3 == 0 && n != 3))
+        return false;
+    if (n <= 3)
+        return true;
+ 
+    lint d = n / 2;
+    lint s = 1;
+    while (!(d & 1)) {
+        d /= 2;
+        ++s;
+    }
+ 
+    if (n < 1373653)
+        return witness(n, s, d, 2) && witness(n, s, d, 3);
+    if (n < 9080191)
+        return witness(n, s, d, 31) && witness(n, s, d, 73);
+    if (n < 4759123141)
+        return witness(n, s, d, 2) && witness(n, s, d, 7) && witness(n, s, d, 61);
+    if (n < 1122004669633)
+        return witness(n, s, d, 2) && witness(n, s, d, 13) && witness(n, s, d, 23) && witness(n, s, d, 1662803);
+    if (n < 2152302898747)
+        return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11);
+    if (n < 3474749660383)
+        return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11) && witness(n, s, d, 13);
+    return witness(n, s, d, 2) && witness(n, s, d, 3) && witness(n, s, d, 5) && witness(n, s, d, 7) && witness(n, s, d, 11) && witness(n, s, d, 13) && witness(n, s, d, 17);
+}
---
+++ trinomials_ADA (3).cpp
@@ -0,0 +1,168 @@
+
+#include "is_prime.h"
+#include <vector>
+#include <iostream>
+#include <set>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include "boost/math/common_factor_rt.hpp"
+
+#define debug(x) cerr << #x << " -> " << (x) << "\n"; 
+using namespace std;
+typedef long long lint;
+
+//------------------------------------------------------------------------------
+//utility functions
+
+//compute exponents by repeated squaring
+lint powm(lint b, lint r, lint p) {
+  r = r % (p - 1);
+  const bitset<64> pow2(r);
+  lint result = 1;
+  lint count = 0;
+  for (int i = 0; count < pow2.count(); ++i) {
+    if (pow2.test(i)) {
+      ++count;
+      lint a = b;
+      for (int j = 0; j < i; ++j) {
+        a = (a * a) % p;
+      }
+      result = (result * a) % p;
+    }
+  }
+  return result;
+}
+
+lint next_prime(lint p) {
+  p += 2; 
+  while (!is_prime_mr(p)) {
+    p += 2;
+  }
+  return p;
+}
+
+//------------------------------------------------------------------------------
+//trinomial testing
+
+//global data for all parallel threads
+vector<lint> A;
+vector<lint> B;
+lint kiran_A;
+lint degree;
+lint mult_d;
+
+lint trinomial_count(lint p) {
+  lint c = 1;
+  degree = -1;
+  fill(A.begin(), A.end(), 0);  
+  while (A.size() < p) {
+    A.push_back(0);
+    B.push_back(0);
+  }
+  
+  for (lint n = 2; n < (p - 1) / 2 + 2; ++n) {
+    lint cn = 1;
+    #pragma omp parallel
+    {
+
+      #pragma omp for
+      for (lint i = 0; i < p; ++i) {
+        B[i] = 1;
+      }
+
+      #pragma omp for
+      for (lint i = 2; i < p; ++i) {
+        lint next = (A[i] - 1); 
+        if (next == -1)
+          next = p - 1;         
+        next *= i;              
+        A[i] = next % p;       
+        
+        #pragma omp atomic
+          ++B[A[i]];            
+      }
+      
+
+      #pragma omp for reduction(max : cn)
+      for (lint i = 2; i < p; ++i) {
+        if (B[i] > cn) {
+          cn = B[i];
+        }
+      }
+    } //end parallel section
+
+    if (cn > c) {
+      c = cn;
+      degree = n;
+      mult_d = 1;
+      kiran_A = -1;
+	  
+      #pragma omp parallel for
+      for (int i = 2; i < p; ++i) {
+        if (B[i] == cn) {
+          if (kiran_A == -1)
+            kiran_A = i;
+        }
+      }	//end parallel section
+    }
+    else if (cn == c) {
+      ++mult_d;
+    }
+  }
+
+  return c;
+}
+
+void print_data(lint p, lint n, ostream& out = cout) {
+  //p prime
+  //n = maximal roots of a trinomial
+  //kiran_A = kiran's "A" parameter
+  //d = minimal degree of trinomial with n roots
+  //mult_d = number of distinct degrees that admit n roots
+  out << p << "\t\t " << n << "\t " << kiran_A << "\t\t " <<"\t " << degree << "\t\t " << mult_d * 2 << "\n"; 
+}
+
+//begin testing at Fp; ignore all results of N or less
+void search(lint p, lint N) {
+  set<int> n_found;
+  for (lint n = 0; n <= N; ++n)
+    n_found.insert(n);
+
+  while (true) {
+    lint n = trinomial_count(p);
+    ofstream ofs2("trinomials.txt", fstream::app);
+    print_data(p, n, ofs2);
+    //print_data(p, n);
+    ofs2.close();
+
+    if (n_found.count(n) == 0) {
+      n_found.insert(n);
+      ofstream ofs("extremal_pn.txt", fstream::app);
+      print_data(p, n, ofs);
+      print_data(p, n);
+    }
+
+    p = next_prime(p);
+  }
+}
+
+//------------------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+  lint P, N;
+  if (argc >= 2) P = stoi(argv[1]);
+  else P = 5;
+  if (argc == 3) N = stoi(argv[2]);
+  else N = 1;
+  
+  ofstream ofs("threads used.txt", fstream::trunc);
+  #pragma omp parallel
+  {
+    ofs << "x";
+  }
+  ofs.close();
+
+  search(P, N);
+}
